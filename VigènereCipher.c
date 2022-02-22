60280c6aa19217bc3de3e0589582cf2421dfb78e
Poojan Yagnik <65134727+pyagnik3@users.noreply.github.com>
2022-02-22 13:59:15 -0500

    Add files via upload

---
+++ VigènereCipher.c
@@ -0,0 +1,34 @@
+/*=============================================================================
+| Assignment: pa01 - Encrypting a plaintext file using the Vigenere cipher
+|
+| Author: Poojan Yagnik
+| Language: c
+|
+| To Compile: gcc -o pa01 pa01.c
+|
+| To Execute: c -> ./pa01 kX.txt pX.txt
+|                   where kX.txt is the keytext file
+|                   and pX.txt is plaintext file
+|
+| Note: All input files are simple 8 bit ASCII input
+|
+| Class: CIS3360 - Security in Computing - Spring 2022
+| Instructor: McAlpin
+| Due Date: 2/27/22
+|
++=============================================================================*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+
+/*=============================================================================
+| I [Poojan Yagnik] [PO395937] affirm that this program is
+| entirely my own work and that I have neither developed my code together with
+| any another person, nor copied any code from any other person, nor permitted
+| my code to be copied or otherwise used by any other person, nor have I
+| copied, modified, or otherwise used programs created by others. I acknowledge
+| that any violation of the above terms will be treated as academic dishonesty.
++=============================================================================*/
